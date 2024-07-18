9a864787e071b590b801c96ac4292777be1f2834
Teboho <drmosuhli@gmail.com>
2024-07-18 16:40:03 +0200

    create user and login routes with error handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 })
 
+app.use("/api/auth", authRouter);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
