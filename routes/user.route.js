23ee1b6d53c95fdcead4511630889d583100b5a6
Teboho <drmosuhli@gmail.com>
2024-07-19 20:52:41 +0200

    add get user by ID endpoint

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,6 +3,7 @@ import {
   deleteUser,
   updateUser,
   getUsers,
+  getUserById,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/list", getUsers);
 router.patch("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/user-info/:id", verifyToken, getUserById);
 
 export default router;
