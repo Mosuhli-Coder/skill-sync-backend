4006b4e2546bf62eb86b13be2ca9b73aa871cbeb
Teboho <drmosuhli@gmail.com>
2024-08-14 12:57:09 +0200

    set cookies with lax not Strict

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -5,11 +5,10 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   console.log("token: ", token);
   
-  const token1 = req.cookies;
-  console.log("token1: ", token1);
   if (!token) {
     return next(errorHandler(401, "Unauthorized"));
   }
+
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return next(errorHandler(403, "Forbidden"));
